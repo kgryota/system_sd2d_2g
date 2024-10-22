56f294c903ab5ba43c6a58dacf21d743a76cc1fd
kgryota <2301159@s.asojuku.ac.jp>
2024-10-22 12:43:21 +0900

    tmp1022

---
+++ testpage/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <!--googleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+
+    <title>乾杯市場 ～全国のお酒を販売～</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <a class="site-title" href="../">
+                <h5>全国のお酒が楽しめる</h5>
+                <h1>乾杯市場</h1>
+            </a>
+            <div class="header-menu">
+                <a class="header-menu-btn" href="../search"><img src="../assets/img/menu/search.svg"></a>
+                <a class="header-menu-btn" href="../user"><img src="../assets/img/menu/user.svg"></a>
+                <a class="header-menu-btn" href="../cart"><img src="../assets/img/menu/cart.svg"></a>
+            </div>
+        </div>
+    </header><!--ヘッダー-->
+    <div class="content-area">
+        <button id="" class="btn">
+            <p>テンプレート文</p>
+        </button>
+        <button id="" class="btn2">
+            <p>テンプレート文</p>
+        </button>
+        <input type="text" name="" class="forminput1" placeholder="テンプレート文">
+
+        <select name="" class="selectstyle">
+            <option value="">テスト</option>
+            <option value="">テスト</option>
+        </select>
+    </div>
+</body>
+</html>
