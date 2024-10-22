14f44dbe7cd97df3dbe9506bd4feabd362fe24ff
ryota041204 <2301159@s.asojuku.ac.jp>
2024-10-22 11:29:32 +0900

    テンプレート

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>乾杯市場 ～全国のお酒を販売～</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <div class="site-title">
+                <h5>全国のお酒が楽しめる</h5>
+                <h1>乾杯市場</h1>
+            </div>
+            <div class="header-menu">
+                <a class="header-menu-btn" href="#"><img src="assets/img/menu/search.svg"></a>
+                <a class="header-menu-btn" href="#"><img src="assets/img/menu/user.svg"></a>
+                <a class="header-menu-btn" href="#"><img src="assets/img/menu/cart.svg"></a>
+            </div>
+        </div>
+    </header><!--ヘッダー-->
+    <div class="content-area">
+
+    </div>
+</body>
+</html>
