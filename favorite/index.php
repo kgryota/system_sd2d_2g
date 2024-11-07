ab582f4448f41be46adb3eb3b3f1ac49286457c0
kentoAkama <2301122@s.asojuku.ac.jp>
2024-11-07 12:39:57 +0900

    好みのお酒

---
+++ favorite/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/page.css">
+
+    <!--googleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+
+    <title>乾杯市場 ～全国のお酒を販売～</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <a class="site-title" href="../">
+                <h5>全国のお酒が楽しめる</h5>
+                <h1>乾杯市場</h1>
+            </a>
+            <div class="header-menu">
+                <a class="header-menu-btn" href="../search"><img src="../assets/img/menu/search.svg"></a>
+                <a class="header-menu-btn" href="../user"><img src="../assets/img/menu/user.svg"></a>
+                <a class="header-menu-btn" href="../cart"><img src="../assets/img/menu/cart.svg"></a>
+            </div>
+        </div>
+    </header><!--ヘッダー-->
+    <div class="content-area">
+    <h2 class="page-title">性別を教えてください</h2><br>
+    <div class="favorite-sei">
+    <input type="radio" name="sei" value="man">男性
+    <input type="radio" name="sei" value="woman">女性<br>
+    <input type="radio" name="sei" value="other">その他
+    <input type="radio" name="sei" value="not">回答しない<br>
+    </div>
+    <h2 class="page-title">好きなお酒のカテゴリーを教えてください</h2><br>
+    <p>*複数回答可</p>
+    <input type="checkbox" name="cotegory" value="brrr">ビール<br>
+    <input type="checkbox" name="cotegory" value="brrr">ビール<br>
+    <input type="checkbox" name="cotegory" value="brrr">ビール<br>
+    <input type="checkbox" name="cotegory" value="brrr">ビール<br>
+    <input type="checkbox" name="cotegory" value="brrr">ビール<br>
+    <button id="" class="btn">
+            <p>登録</p>
+        </button>
+    </div>
+</body>
+</html>
