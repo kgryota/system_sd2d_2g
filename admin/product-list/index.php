575c58e418e29fabef09872a939fe552e866b9cf
kentoAkama <2301122@s.asojuku.ac.jp>
2024-11-13 11:34:55 +0900

    商品登録一覧

---
+++ admin/product-list/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/page.css">
+
+    <!--googleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+
+    <title>乾杯市場 ～全国のお酒を販売～</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <a class="site-title" href="../">
+                <h5>全国のお酒が楽しめる</h5>
+                <h1>乾杯市場</h1>
+            </a>
+            <div class="header-menu">
+                <a class="header-menu-btn" href="../../search"><img src="../../assets/img/menu/search.svg"></a>
+                <a class="header-menu-btn" href="../../user"><img src="../../assets/img/menu/user.svg"></a>
+                <a class="header-menu-btn" href="../../cart"><img src="../../assets/img/menu/cart.svg"></a>
+            </div>
+        </div>
+    </header><!--ヘッダー-->
+    <div class="content-area">
+    <div class="container">
+        <div class="header">
+            <button class="header-button">注文情報</button>
+            <button class="header-button">新規登録</button>
+        </div>
+
+        <div class="product-list">
+            <!-- Repeat this block for each product -->
+            <div class="product-item">
+                <div class="product-info">
+                    <p>商品ID：〇〇</p>
+                    <p>商品名：〇〇</p>
+                </div>
+                <div class="product-buttons">
+                    <button type="button" class="button-update">更新</button>
+                    <button type="button" class="button-add">在庫追加</button>
+                </div>
+            </div>
+            <!-- End of product block -->
+        </div>
+        <div class="product-list">
+            <!-- Repeat this block for each product -->
+            <div class="product-item">
+                <div class="product-info">
+                    <p>商品ID：〇〇</p>
+                    <p>商品名：〇〇</p>
+                </div>
+                <div class="product-buttons">
+                    <button type="button" class="button-update">更新</button>
+                    <button type="button" class="button-add">在庫追加</button>
+                </div>
+            </div>
+            <!-- End of product block -->
+        </div>
+        <div class="product-list">
+            <!-- Repeat this block for each product -->
+            <div class="product-item">
+                <div class="product-info">
+                    <p>商品ID：〇〇</p>
+                    <p>商品名：〇〇</p>
+                </div>
+                <div class="product-buttons">
+                    <button type="button" class="button-update">更新</button>
+                    <button type="button" class="button-add">在庫追加</button>
+                </div>
+            </div>
+            <!-- End of product block -->
+        </div>
+        <div class="product-list">
+            <!-- Repeat this block for each product -->
+            <div class="product-item">
+                <div class="product-info">
+                    <p>商品ID：〇〇</p>
+                    <p>商品名：〇〇</p>
+                </div>
+                <div class="product-buttons">
+                    <button type="button" class="button-update">更新</button>
+                    <button type="button" class="button-add">在庫追加</button>
+                </div>
+            </div>
+            <!-- End of product block -->
+        </div>
+    </div>
+
+        
+    </div>
+</body>
+</html>
