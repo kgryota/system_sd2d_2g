51c9049d335f3f5aaa79c8bd2fcee17cba424eee
kentoAkama <2301122@s.asojuku.ac.jp>
2024-11-13 12:06:41 +0900

    商品登録

---
+++ admin/product-registration/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/page.css">
+
+    <!--googleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+
+    <title>乾杯市場 ～全国のお酒を販売～</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <a class="site-title" href="../">
+                <h5>全国のお酒が楽しめる</h5>
+                <h1>乾杯市場</h1>
+            </a>
+            <div class="header-menu">
+                <a class="header-menu-btn" href="../../search"><img src="../../assets/img/menu/search.svg"></a>
+                <a class="header-menu-btn" href="../../user"><img src="../../assets/img/menu/user.svg"></a>
+                <a class="header-menu-btn" href="../../cart"><img src="../../assets/img/menu/cart.svg"></a>
+            </div>
+        </div>
+    </header><!--ヘッダー-->
+    <div class="content-area">
+    <h1 class="page-title">商品登録</h1><br>
+   <p><h2>商品画像</h2></p>
+    <form action="../login-complete/index.php" method="post">
+        <input type="file" name="product">
+        <input type="text" name="product_name" class="forminput1" placeholder="商品名">
+        <input type="text" name="price" class="forminput1" placeholder="価格">
+        <input type="text" name="explanation" class="forminput1" placeholder="説明">
+        <button id="" class="btn">
+            <p>登録</p>
+        </button>
+    </form>
+
+    <?php
+        $err = $_GET['err'];
+        if($err){
+            echo '<p class="error-message">エラー：IDまたはパスワードが違います。</p>';
+        }
+    ?>
+    
+
+        
+    </div>
+</body>
+</html>
